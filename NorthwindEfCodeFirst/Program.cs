1b8f3f8bb4cef6daa4c2eafaaab21ca0ddd8a0d4
batuhanileri <bileriofficial@gmail.com>
2021-10-04 10:49:09 +0300

    Code First Add,Delete,Update

--- NorthwindEfCodeFirst/Program.cs
+++ NorthwindEfCodeFirst/Program.cs
@@ -1,4 +1,5 @@
 ﻿using NorthwindEfCodeFirst.Contexts;
+using NorthwindEfCodeFirst.Entitites;
 using System;
 
 namespace NorthwindEfCodeFirst
@@ -7,16 +8,67 @@ namespace NorthwindEfCodeFirst
     {
         static void Main(string[] args)
         {
-            using (var northwindContext=new NorthwindContext())
+            // One();
+            // Two();
+            // Three();
+            // Four();
+            Console.ReadLine();
+
+        }
+
+        private static void Four()
+        {
+            using (var northwindContext = new NorthwindContext())
+            {
+                Order order = northwindContext.Orders.Find(1);
+                northwindContext.Orders.Remove(order);
+                northwindContext.SaveChanges();
+            }
+        }
+
+        private static void Three()
+        {
+            using (var northwindContext = new NorthwindContext())
+            {
+                Customer customer = northwindContext.Customers.Find("Batuhan");//primary keye göre buluyor 
+                customer.Orders.Add(new Order
+                {
+                    OrderID = 1,
+                    OrderDate = DateTime.Now,
+                    ShipCity = "Ankara",
+                    ShipCountry = "Türkiye"
+                });
+                northwindContext.SaveChanges();
+            }
+        }
+
+        private static void Two()
+        {
+            using (var northwindContext = new NorthwindContext())
+            {
+                var customer = new Customer
+                {
+                    CustomerID = "Batuhan",
+                    City = "Bursa",
+                    CompanyName = "batuhan.com",
+                    ContactName = "Batuhan İleri",
+                    Country = "Türkiye"
+                };
+                northwindContext.Customers.Add(customer);
+                northwindContext.SaveChanges();
+            }
+        }
+
+        private static void One()
+        {
+            using (var northwindContext = new NorthwindContext())
             {
                 foreach (var item in northwindContext.Customers)
                 {
-                    Console.WriteLine("Customer Name : {0}",item.ContactName);
+                    Console.WriteLine("Customer Name : {0}", item.ContactName);
                 }
 
             }
-            Console.ReadLine();
-
         }
     }
 }
